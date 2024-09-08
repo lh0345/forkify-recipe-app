dc836e8cb512ca2659a67bfe4035e410fe551892
lh0345 <114766163+lh0345@users.noreply.github.com>
2024-09-08 02:21:58 +0200

    refixing api call and search css

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,25 +1,38 @@
-class SearchView{
-    #parentEl = document.querySelector('.search');
+class SearchView {
+  #parentEl = document.querySelector('.search');
+  #allowedQueries = [
+    "carrot", "broccoli", "asparagus", "cauliflower", "corn", "cucumber", 
+    "green pepper", "lettuce", "mushrooms", "onion", "potato", "pumpkin", 
+    "red pepper", "tomato", "beetroot", "brussel sprouts", "peas", "zucchini", 
+    "radish", "sweet potato", "artichoke", "leek", "cabbage", "celery", "chili", 
+    "garlic", "basil", "coriander", "parsley", "dill", "rosemary", "oregano", 
+    "cinnamon", "saffron", "green bean", "bean", "chickpea", "lentil", "apple", 
+    "apricot", "avocado", "banana", "blackberry", "blackcurrant", "blueberry", 
+    "boysenberry", "cherry", "coconut", "fig", "grape", "grapefruit", "kiwifruit", 
+    "lemon", "lime", "lychee", "mandarin", "mango", "melon", "nectarine", "orange", 
+    "papaya", "passion fruit", "peach", "pear", "pineapple", "plum", "pomegranate", 
+    "quince", "raspberry", "strawberry", "watermelon", "salad", "pizza", "pasta", 
+    "popcorn", "lobster", "steak", "bbq", "pudding", "hamburger", "pie", "cake", 
+    "sausage", "tacos", "kebab", "poutine", "seafood", "chips", "fries", "masala", 
+    "paella", "som tam", "chicken", "toast", "marzipan", "tofu", "ketchup", "hummus", 
+    "chili", "maple syrup", "parma ham", "fajitas", "champ", "lasagna", "poke", 
+    "chocolate", "croissant", "arepas", "bunny chow", "pierogi", "donuts", "rendang", 
+    "sushi", "ice cream", "duck", "curry", "beef", "goat", "lamb", "turkey", "pork", 
+    "fish", "crab", "bacon", "ham", "pepperoni", "salami", "ribs"
+  ];
 
-    getQuery(){
-        const query = this.#parentEl.querySelector('.search__field').value;
-        return query;
-    }
-    
-    addHandlerSearch(handler) {
-        this.#parentEl.querySelector('.search__field').addEventListener('input', function (e) {
-          e.preventDefault();
-          handler();
-        });
-      }
-    }
+  getQuery() {
+    const query = this.#parentEl.querySelector('.search__field').value.trim().toLowerCase();
+    return this.#allowedQueries.includes(query) ? query : null;
+  }
+
+  addHandlerSearch(handler) {
+    this.#parentEl.querySelector('.search__field').addEventListener('input', function (e) {
+      e.preventDefault();
+      const query = this.getQuery();
+      if (query) handler(query); // Call handler only if query matches the allowed list
+    }.bind(this));
+  }
+}
+
 export default new SearchView();
-
-
-    // addHandlerSearch(handler){
-    //     this.#parentEl.addEventListener('submit', function (e){
-    //     e.preventDefault();
-    //     handler();
-    //     })
-    // }
-
